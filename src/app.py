cd58210918f88fe9816507f2ee2481f8dcc5b3b6
benjalonso <alonsobenjaminlb@gmail.com>
2022-10-02 19:36:52 +0000

    listo

--- src/app.py
+++ src/app.py
@@ -16,14 +16,21 @@ CORS(app)
 jackson_family = FamilyStructure("Jackson")
 
 # Handle/serialize errors like a JSON object
+
+
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
 
 # generate sitemap with all your endpoints
+
+
 @app.route('/')
 def sitemap():
     return generate_sitemap(app)
+
+# GET
+
 
 @app.route('/members', methods=['GET'])
 def handle_hello():
@@ -35,8 +42,52 @@ def handle_hello():
         "family": members
     }
 
-
     return jsonify(response_body), 200
+
+# POST
+
+
+@app.route('/member', methods=['POST'])
+def add_member():
+    member = request.json
+
+    if not member:
+        return jsonify({"Message": "Not a valid member!"}), 400
+    jackson_family.add_member(member)
+    return jsonify({"Message": "Member added correctly"}), 200
+
+# DELETE
+
+
+@app.route('/member/<int:member_id>', methods=['DELETE'])
+def delete_member(member_id):
+    member = jackson_family.delete_member(member_id)
+    if not member:
+        return jsonify({"Message": "ID doesnt exist"})
+    print("Miembro eliminado!!")
+    return jsonify(member)
+
+
+# GET by ID
+@app.route('/member/<int:member_id>', methods=['GET'])
+def get_member(member_id):
+    member = jackson_family.get_member(member_id)
+    if member:
+        return jsonify(member), 200
+    else:
+        return jsonify({"Message": "Member doesn't exist"}), 400
+
+# UPDATE
+
+
+@app.route('/member/<int:member_id>', methods=['PUT'])
+def update_member(member_id):
+    member = request.json
+    if not member:
+        return jsonify({"Message": "Valid member needed"}), 400
+    jackson_family.update_member(member_id, member)
+    return jsonify(member)
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
